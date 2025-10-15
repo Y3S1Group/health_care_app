63fd4c38957977e26af663608faaf9d086e9a630
ravindu422 <thiranjayaravindu@gmail.com>
2025-10-15 16:38:16 +0530

    created proxy to connect frontend with backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5009',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@/components': path.resolve(__dirname, './src/components'),
+      '@/services': path.resolve(__dirname, './src/services'),
+      '@/hooks': path.resolve(__dirname, './src/hooks'),
+      '@/types': path.resolve(__dirname, './src/types')
+    }
+  }
 })
