08bdf0eb78a34b77bf53d76267e306d3e08db820
hiruvindajayashanka2001 <hiruvindajayashanka2001@gmail.com>
2025-10-17 22:40:22 +0530

    created test-cases for testing

---
+++ backend/jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/server.ts',
+    '!src/app.ts',
+  ],
+  coverageDirectory: 'coverage',
+  verbose: true,
+  forceExit: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+};
